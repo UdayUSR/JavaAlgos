88a38d9abf16775dbdbba8bef652810d33f93880
UdayUSR <udayfkbd@gmail.com>
2022-05-05 18:00:05 +0600

    modified classs of b1 from b2

--- src/secondbranchclass/secondBranchClass.java
+++ src/secondbranchclass/secondBranchClass.java
@@ -1,11 +1,13 @@
 public class stud2{
     String name, hometown;
     int roll;
+    double cgpa;
 
     public stud2(){
         name="Uday";
         hometown="Bagerhat";
         roll=1807106;
+        cgpa=3.5;
     }
     public String getName() {
         return name;
@@ -25,4 +27,11 @@ public class stud2{
     public void setRoll(roll){
         this.roll=roll;
     }
+    public double getcgpa() {
+        return cgpa;
+    }
+    public void setcgpa() {
+        this.cgpa=cgpa;
+    }
+
 }
