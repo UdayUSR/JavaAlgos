cfb696cd0915655b8132736b57c49cad5761bfbf
UdayUSR <udayfkbd@gmail.com>
2022-05-05 17:46:49 +0600

    Second Class added in b1

---
+++ src/secondbranchclass/secondBranchClass.java
@@ -0,0 +1,28 @@
+public class stud2{
+    String name, hometown;
+    int roll;
+
+    public stud2(){
+        name="Uday";
+        hometown="Bagerhat";
+        roll=1807106;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(nm){
+        this.name=nm;
+    }
+    public String getHometown() {
+        return hometown;
+    }
+    public void setHometown(hometown){
+        this.hometown=hometown;
+    }
+    public int getRoll() {
+        return roll;
+    }
+    public void setRoll(roll){
+        this.roll=roll;
+    }
+}
