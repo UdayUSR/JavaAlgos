8beceffb9107fcaec1254a463399f61e982c6234
UdayUSR <udayfkbd@gmail.com>
2022-05-05 17:27:03 +0600

    First Class added in Master

---
+++ src/mainbranchclass/mainBranchClass.java
@@ -0,0 +1,19 @@
+public class Stud1(){
+
+    String name;
+
+    public Stud1(){
+
+        name="Uday";
+    }
+
+    public String getName() {
+
+        return name; 
+    }
+
+    public void setName(String nm){
+        this.name=nm;
+    }
+
+}
